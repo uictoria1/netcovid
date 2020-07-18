fbef9d802293004218b719ab693cd2d8f3d3150c
20162011 <20162011@unist.ac.kr>
2020-07-18 18:22:52 +0600

    added template/index.html

--- netcovid/urls.py
+++ netcovid/urls.py
@@ -17,5 +17,7 @@ from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
+    path('', TemplateView.as_view(template_name='index.html')),
+
     path('admin/', admin.site.urls),
 ]
